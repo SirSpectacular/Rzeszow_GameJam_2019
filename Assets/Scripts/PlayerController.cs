ebfe7f4477d2994516a933d38770b02c5ab7eb6e
Esmfan <zbigniew.kaczynski@op.pl>
2019-10-25 22:55:22 +0200

    PlayerMaterial

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -10,15 +10,16 @@ namespace PlatformShift.Controll {
 
         float horizontalInput = 0f;
         bool jumpInput = false;
-
+        bool jumpPressed = false;
 
 
         private void Update() {
             jumpInput = Input.GetButtonDown("Jump");
             horizontalInput = Input.GetAxis("Horizontal");
+            jumpPressed = Input.GetButton("Jump");
         }
         private void FixedUpdate() {
-            mover.Move(horizontalInput * runSpeed, jumpInput);
+            mover.Move(horizontalInput * runSpeed, jumpInput,jumpPressed);
             jumpInput = false;
         }
     }
