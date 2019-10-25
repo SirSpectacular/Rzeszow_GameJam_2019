2c8531dddd0e6f863cdf62ad8e0fe599c81dc936
Szymon Czarny <sz.p.czarny@gmail.com>
2019-10-25 22:27:31 +0200

    Added platform object and move patterns

---
+++ Assets/Scripts/Platform/MovePatterns/CircularMovePattern.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CircularMovePattern : MovePattern
+{
+    public Transform  rotateAround;
+
+    public override void step(float stepSize)
+    {
+        transform.RotateAround(rotateAround.position, new Vector3(0,0,1), stepSize);
+        transform.eulerAngles = Vector3.zero;
+    }    
+}
---
+++ Assets/Scripts/Platform/PlatformMovement.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformMovement : MonoBehaviour
+{
+    public MovePattern movePattern;
+    public float timeScale;
+    public bool freezed;
+    public float baseSpeed;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    private void FixedUpdate()
+    {
+        if(!freezed)
+            movePattern.step(Time.fixedDeltaTime * timeScale * baseSpeed);
+    }
+
+
+}
