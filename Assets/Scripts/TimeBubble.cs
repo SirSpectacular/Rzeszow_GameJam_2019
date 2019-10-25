2b8d90264cab4f4de6485e6a1634e93ad0cc76a6
BlazingBuddy <maksszaszkow@gmail.com>
2019-10-26 01:27:52 +0200

    Time bubble

---
+++ Assets/Scripts/TimeBubble.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimeBubble : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public GameState gameState;
+    private List<GameObject> platforms;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        gameObject.transform.position = new Vector3(Camera.main.ScreenToWorldPoint(Input.mousePosition).x, Camera.main.ScreenToWorldPoint(Input.mousePosition).y, 0);
+        gameObject.transform.Rotate(0, 0, -10*Time.deltaTime);
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        collision.gameObject.GetComponent<PlatformMovement>().timeScale = gameState.timeScale;
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        collision.gameObject.GetComponent<PlatformMovement>().timeScale = 1;
+    }
+}
