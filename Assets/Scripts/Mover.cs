630f17bd440e9507253a44f4365f176356673026
Esmfan <zbigniew.kaczynski@op.pl>
2019-10-25 21:15:46 +0200

    BasicMove

---
+++ Assets/Scripts/Mover.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace PlatformShift.Move {
+
+
+    public class Mover : MonoBehaviour {
+
+        [SerializeField] float jumpForce = 1000000;
+        [SerializeField] float smoothTime = 0.03F;
+
+
+        private bool isOnPlatform = false;
+        private Rigidbody2D rigidBody = null;
+        private Vector2 currentVelocity = Vector2.zero;
+        
+        void Awake() {
+            rigidBody = GetComponent<Rigidbody2D>();
+
+            isOnPlatform = true;
+        }
+
+        void FixedUpdate() {
+
+        }
+
+        public void Move(float horizontalInput, bool jumpInput) {
+            
+
+
+
+            Vector2 targetVelocity = new Vector2(horizontalInput, rigidBody.velocity.y);
+
+            rigidBody.velocity =  Vector2.SmoothDamp(rigidBody.velocity, targetVelocity, ref currentVelocity, smoothTime);
+
+
+            if (jumpInput && isOnPlatform) {
+                rigidBody.AddForce(new Vector2(0f, jumpForce));
+                isOnPlatform = false;
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,25 @@
+﻿using PlatformShift.Move;
+using UnityEngine;
+
+namespace PlatformShift.Controll {
+
+    public class PlayerController : MonoBehaviour {
+
+        [SerializeField] Mover mover = null;
+        [SerializeField] float runSpeed = 10f;
+
+        float horizontalInput = 0f;
+        bool jumpInput = false;
+
+
+
+        private void Update() {
+            jumpInput = Input.GetButtonDown("Jump");
+            horizontalInput = Input.GetAxis("Horizontal");
+        }
+        private void FixedUpdate() {
+            mover.Move(horizontalInput * runSpeed, jumpInput);
+            jumpInput = false;
+        }
+    }
+}
