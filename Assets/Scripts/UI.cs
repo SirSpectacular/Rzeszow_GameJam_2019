2ec8c3b6d2f2aca93591943e96b9a37812c003b9
Esmfan <zbigniew.kaczynski@op.pl>
2019-10-26 02:35:43 +0200

    Fader

---
+++ Assets/Scripts/UI.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UI : MonoBehaviour{
+
+    [SerializeField] Fade fade;
+
+    public void BeginGame() {
+        StartCoroutine("SceneTransition");
+    }
+    
+
+    public void Exit() {
+        Application.Quit();
+     
+    }
+ 
+
+    IEnumerator SceneTransition() {
+
+        yield return  fade.FadeOut(1f);
+
+        SceneManager.LoadScene(1);
+    }
+
+
+}
