ce844c7c906af976fa1526ee9e90e6d3608b2f7a
BlazingBuddy <maksszaszkow@gmail.com>
2019-10-26 03:21:29 +0200

    Package0 + horizontal platform

---
+++ Assets/Scripts/HorizontalPlatform.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HorizontalPlatform : MonoBehaviour
+{
+
+    public int side;
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        collision.gameObject.GetComponent<Rigidbody2D>().velocity = new Vector2(0, 0);
+        collision.gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(600 * side, 600));
+        
+    }
+}
