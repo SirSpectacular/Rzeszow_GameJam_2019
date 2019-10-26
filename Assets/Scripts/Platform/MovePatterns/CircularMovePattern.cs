3f6e6d8761e3488b489bccbf50b77028582db02b
Szymon Czarny <sz.p.czarny@gmail.com>
2019-10-26 11:06:09 +0200

    m

--- Assets/Scripts/Platform/MovePatterns/CircularMovePattern.cs
+++ Assets/Scripts/Platform/MovePatterns/CircularMovePattern.cs
@@ -8,7 +8,12 @@ public class CircularMovePattern : MovePattern
 
     public override void step(float stepSize)
     {
+        Debug.Log(stepSize);
         transform.RotateAround(rotateAround.position, new Vector3(0,0,1), stepSize);
+        Debug.Log(transform);
+
         transform.eulerAngles = Vector3.zero;
-    }    
+        Debug.Log(transform);
+
+    }
 }
